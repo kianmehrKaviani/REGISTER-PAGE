92eff6b19b04369f74a069eac898f0a4ecfb1ca4
kianmehrKaviani <kianmehrkav.1390@gmail.com>
2026-06-30 16:56:21 +0330

    Update connection.php

--- Database/connection.php
+++ Database/connection.php
@@ -1,11 +1,10 @@
 <?php
-$host = 'localhost';
 $dbname = 'register';
 $username = 'root';
 $password = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=127.0.0.1;port=3307;dbname=$dbname;", $username, $password);
     // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     // die("Connection failed: " . $e->getMessage());
